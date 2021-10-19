5b73ced077fda203e10db75a088d09dff3efc05c
Vyacheslav Trufakin <slavyanofil@gmail.com>
2021-10-19 21:21:53 +0400

    Next correction of HW

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -29,14 +29,18 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int currentIndex = 0;
+        int currentIndex = -1;
         for (int i = 0; i < size; i++) {
             if (storage[i].toString().equals(uuid)) {
                 currentIndex = i;
             }
         }
-        System.arraycopy(storage, size - currentIndex, storage, currentIndex, size - currentIndex);
-        size--;
+        if (currentIndex != -1) {
+            System.arraycopy(storage, size - currentIndex, storage, currentIndex, size - currentIndex);
+            size--;
+        } else {
+            System.out.println("Nonexistent identifier. Try again");
+        }
     }
 
     /**
