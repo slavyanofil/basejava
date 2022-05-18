313182f78c942f09edca2cf23dc328eb6f308d18
Vyacheslav Trufakin <slavyanofil@gmail.com>
2022-05-19 02:52:14 +0400

    HW16 correct DateUtil

--- src/com/urise/webapp/util/DateUtil.java
+++ src/com/urise/webapp/util/DateUtil.java
@@ -7,16 +7,17 @@ import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
 
-    public static final LocalDate NOW = LocalDate.of(3000, 1, 1);
+    public static final LocalDate NOW = LocalDate.of(LocalDate.now().getYear(), LocalDate.now().getMonth(), LocalDate.now().getDayOfMonth());
     private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("MM/yyyy");
 
     public static LocalDate of(int year, Month month) {
         return LocalDate.of(year, month, 1);
     }
 
+
     public static String format(LocalDate date) {
         if (date == null) return "";
-        return date.equals(NOW) ? "Сейчас" : date.format(DATE_FORMATTER);
+        return date.isAfter(NOW) ? "Сейчас" : date.format(DATE_FORMATTER);
     }
 
     public static LocalDate parse(String date) {
